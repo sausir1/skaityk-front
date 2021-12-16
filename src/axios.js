bcbfaa3ce906debb8a7a7b8035c3c017fd895252
sausir1 <sirvydas123s@gmail.com>
2021-12-17 01:21:59 +0200

    axios update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://www.raganaitedoli.lt/api/',
     timeout: 3000,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
